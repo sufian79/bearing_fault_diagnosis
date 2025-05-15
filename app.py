d18d8cd470e3e167c295c31784e3f0961044d122
Mohd Sufian Bin Othman <sufian79@graduate.utm.my>
2025-05-15 08:45:08 +0800

    update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def analyze_signal(df, selected_channels, model_name, RPM, Nb, Bd, Pd, beta_deg)
     input_tensor = torch.tensor(signals, dtype=torch.float32).unsqueeze(0)  # [1, C, 1024]
 
     # Dynamically get model class by name from hybrid_models module
-    ModelClass = getattr(hm, model_name+ "_pt")  
+    ModelClass = getattr(hm, model_name+ "1D_pt")  
     model = ModelClass(input_channels=len(selected_indices))
 
     model.load_state_dict(torch.load(f"{model_name}.pt", map_location=torch.device('cpu')))
